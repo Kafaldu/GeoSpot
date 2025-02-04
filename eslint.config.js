d1f05ea1f12c71a24ce2f2bb76d524cab40aa72c
Kafaldu <76752787+Kafaldu@users.noreply.github.com>
2025-02-04 17:03:59 -0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,21 @@
-export default {
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
+import eslint from 'eslint';
+import pluginReact from 'eslint-plugin-react';
+
+const { Linter } = eslint;
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      env: {
+        browser: true,
+        node: true,
+      },
+    },
   },
-  env: {
-    browser: true,
-    node: true,
-  },
-};
+  pluginReact.configs.recommended,
+];
