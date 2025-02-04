c9c65338907f3675958f8eefdb78527a6a2e630f
Kafaldu <76752787+Kafaldu@users.noreply.github.com>
2025-02-04 16:57:01 -0500

    Create eslint.config.js
    trying something i found on youtube

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'eslint';
+
+export default defineConfig({
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+});
