f5e681a0bd0d7bfafb6b72cb113788f907f6389f
Kafaldu <kafaldu2005@gmail.com>
2025-03-02 01:13:12 -0500

    Server is connected, Database is not connected yet. If you want to test backend server connection run npm run dev in root and then click on the port link

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import { connectDB } from "./config/db.js";
-import loginRoutes from "./routes/login.route.js";
+//import loginRoutes from "./routes/login.route.js";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5007;
 
 app.use(express.json());
-app.use("/api/login",loginRoutes);
-
+app.get("/", (req,res) =>
+{
+  res.send("Server is ready");
+});
 app.listen(PORT, () => 
 {    
-  connectDB();
   console.log(`Server started at http://localhost:${PORT}`);
 });
