3356b80e674985f81d51b836e3b60f5aa3f990cf
Kafaldu <76752787+Kafaldu@users.noreply.github.com>
2025-03-02 00:36:39 -0500

    Create login.model.js

---
+++ backend/models/login.model.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const loginSchema = new mongoose.Schema(
+{
+    username:
+    {
+        type: String,
+        required: true
+    },
+    password:
+    {
+        type: String,
+        required: true
+    },
+},
+{
+    timestamps: true
+});
+
+const Login = mongoose.model('Login', loginSchema);
+
+export default Login;
