97f1013790b5b268221cc528c934e80cbc5962e5
Kafaldu <76752787+Kafaldu@users.noreply.github.com>
2025-03-02 00:47:23 -0500

    Update server.js
    Still needs to be updated for frontend service

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-//Will hold the server access
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { connectDB } from "./config/db.js";
+import loginRoutes from "./routes/login.route.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api/login",loginRoutes);
+
+app.listen(PORT, () => 
+{    
+  connectDB();
+  console.log(`Server started at http://localhost:${PORT}`);
+});
