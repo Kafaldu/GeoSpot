cc7085bd1cb93a2ed64a98596b98d3c298efe422
Kafaldu <kafaldu2005@gmail.com>
2025-03-03 11:06:28 -0500

    Added Welcome Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Box } from '@chakra-ui/react';
 import { useColorMode } from '@chakra-ui/react';
 import SignUpPage from './pages/SignUp';
 import LoginPage from './pages/LogIn';
+import WelcomePage from './pages/Welcome';
 
 console.log("App component is rendering...");
 
@@ -13,7 +14,8 @@ function App() {
         <Box minH="100vh" bg={colorMode === "light" ? "gray.200" : "gray.800"}>
             <Routes>
                 <Route path="/signup" element={<SignUpPage />} />
-                <Route path="/" element={<LoginPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/" element={<WelcomePage />} />
             </Routes>
         </Box>
     );
