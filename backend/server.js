e17f778c3fe2b2fd8bc4f920aaa76aec0ebb5cfe
Kafaldu <kafaldu2005@gmail.com>
2025-03-03 13:35:10 -0500

    fixed version conflict

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import userRoutes from "./routes/user.route.js";
-import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-const __dirname = path.resolve();
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(cors());
 app.use("/api/users", userRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use('/assets', express.static(path.join(__dirname, 'frontend', 'dist', 'assets'), {
+    app.use(express.static(path.join(__dirname, "..", "frontend", "dist"), {
         setHeaders: (res, path) => {
             if (path.endsWith('.js')) {
                 res.set('Content-Type', 'application/javascript');
@@ -26,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
     }));
     
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "..", "frontend", "dist", "index.html"));
     });
 }
 
